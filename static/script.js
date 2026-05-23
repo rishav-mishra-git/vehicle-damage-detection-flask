679191a123c725fd4d477c1de7af24d58bba51ad
Rishav <mrishav65@gmail.com>
2026-05-23 15:22:03 +0530

    Add async image upload and damage detection functionality

--- static/script.js
+++ static/script.js
@@ -1,1 +1,55 @@
 
+async function uploadImage() {
+
+    const input = document.getElementById("imageInput");
+
+    const file = input.files[0];
+
+    if (!file) {
+
+        alert("Please select an image");
+
+        return;
+    }
+
+    document.getElementById("loading").innerHTML =
+        "<p>Detecting damages...</p>";
+
+    const formData = new FormData();
+
+    formData.append("image", file);
+
+    const response = await fetch("/predict", {
+
+        method: "POST",
+        body: formData
+    });
+
+    const data = await response.json();
+
+    let html = "";
+
+    html += `
+        <h2>
+            Total Estimated Cost:
+            ₹${data.total_cost}
+        </h2>
+    `;
+
+    data.damages.forEach(d => {
+
+        html += `
+            <p>
+                <strong>${d.damage}</strong>
+                |
+                Confidence: ${d.confidence}
+                |
+                Cost: ₹${d.cost}
+            </p>
+        `;
+    });
+
+    document.getElementById("loading").innerHTML = "";
+
+    document.getElementById("result").innerHTML = html;
+}
